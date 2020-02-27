4bc0cc2d94261f370aa66a95ff3cfebe995c8014
Patrick Clemins <patrick.clemins@uvm.edu>
2020-02-27 12:19:01 -0500

    Updated Qt qmake directory in build_project.sh

--- build_project.sh
+++ build_project.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if "$HOME/Qt/5.10.0/ios/bin/qmake" -spec macx-ios-clang CONFIG+=iphonesimulator CONFIG+=simulator MacroinvertebrateV3.pro CONFIG+=qml_debug; then
+if "$HOME/Qt/5.14.1/ios/bin/qmake" -spec macx-ios-clang CONFIG+=iphonesimulator CONFIG+=simulator MacroinvertebrateV3.pro CONFIG+=qml_debug; then
 	say 'QMake complete.'
 else
 	say 'QMake failed.'
